5266470d1adef20fd048d441111d1036107abf2f
Kenan <bary.berdiyev.97@mail.ru>
2023-06-06 14:41:40 +0700

    экспорт компонент

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as Button } from './Button/Button';
 export { default as ButtonIcon } from './ButtonIcon/ButtonIcon';
+export { default as Card } from './Card/Card';
+export { default as Divider } from './Divider/Divider';
+export { default as ProfileInfo } from './ProfileInfo/ProfileInfo';
