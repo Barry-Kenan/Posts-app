b2a4c1533499010fa56b7a69293de14915e9dfa1
Kenan <bary.berdiyev.97@mail.ru>
2023-06-06 14:41:25 +0700

    sidebar

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -1,5 +1,7 @@
+import { ProfileInfo } from '@/components';
 import cn from 'classnames';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { FC } from 'react';
 import styles from './Sidebar.module.scss';
 import { SidebarProps } from './Sidebar.props';
@@ -7,13 +9,25 @@ import PostIcon from './post.svg';
 import UserIcon from './user.svg';
 
 const Sidebar: FC<SidebarProps> = ({ className, ...props }) => {
+	const router = useRouter();
 	return (
 		<div className={cn(className, styles.sidebar)} {...props}>
-			<Link href='/' className={styles.item}>
+			<ProfileInfo className={styles.profileInfo} />
+			<Link
+				href='/'
+				className={cn(styles.item, {
+					[styles.active]: router.asPath == '/'
+				})}
+			>
 				<UserIcon />
 				Обо мне
 			</Link>
-			<Link href='/posts' className={styles.item}>
+			<Link
+				href='/posts'
+				className={cn(styles.item, {
+					[styles.active]: router.asPath == '/posts'
+				})}
+			>
 				<PostIcon />
 				Список постов
 			</Link>
