6476ec31f94a21f965455c8fb2b947fe40d9c22f
Kenan <bary.berdiyev.97@mail.ru>
2023-06-07 11:53:35 +0700

    страница профиля

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,43 @@
+import { useActions } from '@/hooks/actions';
+import { useAppSelector } from '@/hooks/redux';
+import { IPost } from '@/interfaces/post.interface';
+import { withLayout } from '@/layout/Layout';
+import { ProfilePage } from '@/page-components';
+import axios from 'axios';
+import { GetStaticProps } from 'next';
 import { Inter } from 'next/font/google';
+import { FC, useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
-	return <h1>Hello Next</h1>;
+const Home: FC<HomeProps> = () => {
+	const { getProfile } = useActions();
+	const { profile, isLoading } = useAppSelector(state => state.profile);
+
+	useEffect(() => {
+		getProfile();
+	}, []);
+	return (
+		<>
+			<ProfilePage />
+			{profile?.name}
+		</>
+	);
+};
+
+export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const { data: posts } = await axios.get<IPost[]>(
+		'https://jsonplaceholder.typicode.com/posts'
+	);
+	return {
+		props: {
+			posts
+		}
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+	posts: IPost[];
 }
