03211232d7fd13ba81bae8ea123adb21c96750ad
Kenan <bary.berdiyev.97@mail.ru>
2023-06-07 11:53:54 +0700

    экспорт компонент

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,6 @@ export { default as Button } from './Button/Button';
 export { default as ButtonIcon } from './ButtonIcon/ButtonIcon';
 export { default as Card } from './Card/Card';
 export { default as Divider } from './Divider/Divider';
+export { default as Post } from './Post/Post';
 export { default as ProfileInfo } from './ProfileInfo/ProfileInfo';
+export { default as Review } from './Review/Review';
