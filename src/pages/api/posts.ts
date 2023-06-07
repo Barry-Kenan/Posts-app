acb211fc8f7939f96d20f1b1d12a69f88175956c
Kenan <bary.berdiyev.97@mail.ru>
2023-06-07 11:47:40 +0700

    получение постов по userId

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -7,18 +7,29 @@ export default async function handler(
 	res: NextApiResponse
 ) {
 	const {
-		query: { limit, page }
+		query: { limit, page, userId }
 	} = req;
 
-	try {
-		const { data } = await axios.get<IPost[]>(
-			'https://jsonplaceholder.typicode.com/posts'
-		);
+	const returnPosts = (data: IPost[]) => {
 		const totalCount = data.length;
 		const from = (Number(page) - 1) * Number(limit);
 		const to = from + Number(limit);
 		const posts = data.slice(from, to);
 		return res.status(200).json({ posts, totalCount });
+	};
+
+	try {
+		if (userId) {
+			const { data } = await axios.get<IPost[]>(
+				`https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+			);
+			returnPosts(data);
+		} else {
+			const { data } = await axios.get<IPost[]>(
+				'https://jsonplaceholder.typicode.com/posts'
+			);
+			returnPosts(data);
+		}
 	} catch (error) {
 		res.json(error);
 	}
