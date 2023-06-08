1eb9c6a594899d853d2b953d61539e79a00d92b9
Kenan <bary.berdiyev.97@mail.ru>
2023-06-08 15:50:17 +0700

    PostPage компонент

--- src/page-components/PostPage/PostPage.props.ts
+++ src/page-components/PostPage/PostPage.props.ts
@@ -1,5 +1,13 @@
 import { IPost } from '@/interfaces/post.interface';
+import { SortType } from '@/interfaces/sort.type';
 
 export interface PostPageProps {
+	sort: SortType;
+	setSort: (sort: SortType) => void;
+	setSearchTitle: (searchTitle: string) => void;
 	posts: IPost[];
+	isLoading: boolean;
+	page: number;
+	setPage: (page: number) => void;
+	pagesCount: number;
 }
--- src/page-components/PostPage/PostPage.tsx
+++ src/page-components/PostPage/PostPage.tsx
@@ -1,9 +1,36 @@
-import Post from '@/components/Post/Post';
+import { PaginationPosts, PostList, Search, SortPosts } from '@/components';
 import { FC } from 'react';
+import styles from './PostPage.module.scss';
 import { PostPageProps } from './PostPage.props';
 
-const PostPage: FC<PostPageProps> = ({ posts }) => {
-	return <>{posts && posts.map(p => <Post key={p.id} post={p} />)}</>;
+const PostPage: FC<PostPageProps> = props => {
+	const {
+		isLoading,
+		page,
+		pagesCount,
+		posts,
+		setPage,
+		setSearchTitle,
+		setSort,
+		sort
+	} = props;
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.searchSort}>
+				<Search className={styles.search} setSearchTitle={setSearchTitle} />
+				<SortPosts sort={sort} setSort={setSort} className={styles.sort} />
+			</div>
+			<PostList posts={posts} isLoading={isLoading} />
+			{posts.length != 0 && (
+				<PaginationPosts
+					className={styles.pagination}
+					page={page}
+					pagesCount={pagesCount}
+					setPage={setPage}
+				/>
+			)}
+		</div>
+	);
 };
 
 export default PostPage;
