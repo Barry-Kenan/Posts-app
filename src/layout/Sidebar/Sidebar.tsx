7fc532aa1d9f4d6a47e8e9a7f38b72effd3ab4a8
Kenan <bary.berdiyev.97@mail.ru>
2023-06-10 11:26:51 +0700

    sidebar

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { ProfileInfo } from '@/components';
+import { useAppSelector } from '@/hooks/redux';
 import cn from 'classnames';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -8,16 +9,15 @@ import { SidebarProps } from './Sidebar.props';
 import PostIcon from './post.svg';
 import UserIcon from './user.svg';
 
-const Sidebar: FC<SidebarProps> = ({ className, isOpened, ...props }) => {
+const Sidebar: FC<SidebarProps> = ({ className, ...props }) => {
 	const router = useRouter();
+	const { profile } = useAppSelector(state => state.profile);
+
 	return (
-		<div
-			className={cn(className, styles.sidebar, {
-				[styles.closed]: !isOpened
-			})}
-			{...props}
-		>
-			<ProfileInfo className={styles.profileInfo} />
+		<div className={cn(className, styles.sidebar)} {...props}>
+			{profile && (
+				<ProfileInfo className={styles.profileInfo} profile={profile} />
+			)}
 			<Link
 				href='/'
 				className={cn(styles.item, {
