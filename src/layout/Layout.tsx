a153cacba59e344d55db36966be6d593f83d4ef2
Kenan <bary.berdiyev.97@mail.ru>
2023-06-10 13:45:41 +0700

    динамический sidebar

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,14 +1,17 @@
+import { Loading } from '@/components';
 import { useActions } from '@/hooks/actions';
+import { useAppSelector } from '@/hooks/redux';
 import { store } from '@/store';
 import cn from 'classnames';
+import dynamic from 'next/dynamic';
 import { Roboto } from 'next/font/google';
-import { FunctionComponent, useEffect, useState } from 'react';
+import { FunctionComponent, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import styles from './Layout.module.scss';
 import { LayoutProps } from './Layout.props';
-import Sidebar from './Sidebar/Sidebar';
+// import Sidebar from './Sidebar/Sidebar';
 
 const roboto = Roboto({
 	weight: ['300', '500', '700'],
@@ -19,13 +22,13 @@ const roboto = Roboto({
  *  Для исправление ошибки
  *  Warning Prop `className` did not match
  */
-// const Sidebar = dynamic(() => import('./Sidebar/Sidebar'), {
-// 	ssr: false,
-// 	loading: () => <>Загрузка...</>
-// });
+const Sidebar = dynamic(() => import('./Sidebar/Sidebar'), {
+	ssr: false,
+	loading: () => <Loading />
+});
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
-	const [isOpened, setIsOpened] = useState<boolean>(true);
+	const { isOpened } = useAppSelector(state => state.sidebar);
 	const { getProfile } = useActions();
 	useEffect(() => {
 		getProfile();
@@ -36,11 +39,7 @@ const Layout = ({ children }: LayoutProps): JSX.Element => {
 				[styles.closed]: !isOpened
 			})}
 		>
-			<Header
-				className={styles.header}
-				isOpened={isOpened}
-				setIsOpened={setIsOpened}
-			/>
+			<Header className={styles.header} />
 			<Sidebar className={cn(styles.sidebar)} />
 			<main className={cn(styles.body)}>{children}</main>
 			<Footer className={styles.footer} />
