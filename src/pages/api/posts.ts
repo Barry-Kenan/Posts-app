3d7fcd9879fb9783e2df2dc2a1482b0e5fa33e27
Kenan <bary.berdiyev.97@mail.ru>
2023-06-10 11:25:13 +0700

    сортировка и поиск

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -1,5 +1,5 @@
+import { jsonApi } from '@/helper/api/api';
 import { IPost } from '@/interfaces/post.interface';
-import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
@@ -7,27 +7,36 @@ export default async function handler(
 	res: NextApiResponse
 ) {
 	const {
-		query: { limit, page, userId }
+		query: { limit, page, sort, search, userId }
 	} = req;
 
 	const returnPosts = (data: IPost[]) => {
+		if (search) {
+			const re = new RegExp(search as string, 'i');
+			data = data.filter(e => e.title.search(re) !== -1);
+		}
+
 		const totalCount = data.length;
+		const pagesCount = Math.ceil(totalCount / Number(limit));
 		const from = (Number(page) - 1) * Number(limit);
 		const to = from + Number(limit);
-		const posts = data.slice(from, to);
-		return res.status(200).json({ posts, totalCount });
+
+		let allPosts = [];
+		if (sort == 'asc') {
+			allPosts = data.sort((a, b) => a.id - b.id);
+		} else {
+			allPosts = data.sort((a, b) => b.id - a.id);
+		}
+		const posts = allPosts.slice(from, to);
+		return res.status(200).json({ posts, totalCount, pagesCount });
 	};
 
 	try {
 		if (userId) {
-			const { data } = await axios.get<IPost[]>(
-				`https://jsonplaceholder.typicode.com/posts?userId=${userId}`
-			);
+			const { data } = await jsonApi.getPosts(userId as string);
 			returnPosts(data);
 		} else {
-			const { data } = await axios.get<IPost[]>(
-				'https://jsonplaceholder.typicode.com/posts'
-			);
+			const { data } = await jsonApi.getPosts();
 			returnPosts(data);
 		}
 	} catch (error) {
