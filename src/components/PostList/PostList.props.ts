db181822265baf4bdc5757b5ffb6096cdb5e519d
Kenan <bary.berdiyev.97@mail.ru>
2023-06-10 11:29:09 +0700

    добавлен classNames и остальные пропсы

--- src/components/PostList/PostList.props.ts
+++ src/components/PostList/PostList.props.ts
@@ -1,6 +1,8 @@
 import { IPost } from '@/interfaces/post.interface';
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
 
-export interface PostListProps {
+export interface PostListProps
+	extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
 	posts: IPost[];
 	isLoading: boolean;
 }
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,23 +1,27 @@
 import { Post } from '@/components';
+import cn from 'classnames';
 import { FC } from 'react';
 import styles from './PostList.module.scss';
 import { PostListProps } from './PostList.props';
 
-const PostList: FC<PostListProps> = ({ posts, isLoading }) => {
+const PostList: FC<PostListProps> = ({
+	posts,
+	isLoading,
+	className,
+	...props
+}) => {
 	if (isLoading) {
-		return <>Загрузка...</>;
+		return <div>Загрузка...</div>;
 	}
 
 	if (posts.length == 0) {
-		return <>Ничего не нашли</>;
+		return <div>Ничего не нашли</div>;
 	}
 
 	return (
-		<>
-			<div className={styles.posts}>
-				{posts.length && posts.map(p => <Post key={p.id} post={p} />)}
-			</div>
-		</>
+		<div className={cn(styles.posts, className)} {...props}>
+			{posts.length && posts.map(p => <Post key={p.id} post={p} />)}
+		</div>
 	);
 };
 
