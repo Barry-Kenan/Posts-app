7b779c71949ce84c6c7a2979f1aa38206793d403
Kenan <bary.berdiyev.97@mail.ru>
2023-06-10 13:45:09 +0700

    закрытие при клике в меленьких экранах

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -1,5 +1,7 @@
 import { ProfileInfo } from '@/components';
+import { useActions } from '@/hooks/actions';
 import { useAppSelector } from '@/hooks/redux';
+import { useResize } from '@/hooks/use-resize';
 import cn from 'classnames';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -12,7 +14,14 @@ import UserIcon from './user.svg';
 const Sidebar: FC<SidebarProps> = ({ className, ...props }) => {
 	const router = useRouter();
 	const { profile } = useAppSelector(state => state.profile);
+	const { setIsOpened } = useActions();
+	const { tablet } = useResize();
 
+	const handleClick = () => {
+		if (tablet) {
+			setIsOpened(false);
+		}
+	};
 	return (
 		<div className={cn(className, styles.sidebar)} {...props}>
 			{profile && (
@@ -23,6 +32,7 @@ const Sidebar: FC<SidebarProps> = ({ className, ...props }) => {
 				className={cn(styles.item, {
 					[styles.active]: router.asPath == '/'
 				})}
+				onClick={handleClick}
 			>
 				<UserIcon />
 				Обо мне
@@ -32,6 +42,7 @@ const Sidebar: FC<SidebarProps> = ({ className, ...props }) => {
 				className={cn(styles.item, {
 					[styles.active]: router.asPath == '/posts'
 				})}
+				onClick={handleClick}
 			>
 				<PostIcon />
 				Список постов
