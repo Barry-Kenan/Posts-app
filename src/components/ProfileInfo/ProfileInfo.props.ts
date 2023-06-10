ef932dd8e46492614383c75b95d4e805f152db1d
Kenan <bary.berdiyev.97@mail.ru>
2023-06-10 11:29:48 +0700

    вывод данных с api

--- src/components/ProfileInfo/ProfileInfo.props.ts
+++ src/components/ProfileInfo/ProfileInfo.props.ts
@@ -1,4 +1,7 @@
+import { IMyProfile } from '@/interfaces/profile.interface';
 import { DetailedHTMLProps, HTMLAttributes } from 'react';
 
 export interface ProfileInfoProps
-	extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
+	extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+	profile: IMyProfile | null;
+}
