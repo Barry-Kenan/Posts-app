4959e0e50e273481e16c423ac2e4d8cb26e0b6d9
Kenan <bary.berdiyev.97@mail.ru>
2023-06-10 11:23:08 +0700

    страница постов определенного пользователя

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,33 +1,54 @@
+import { jsonApi } from '@/helper/api/api';
 import { useActions } from '@/hooks/actions';
 import { useAppSelector } from '@/hooks/redux';
+import { SortType } from '@/interfaces/sort.type';
 import { IUser } from '@/interfaces/users.interface';
 import { withLayout } from '@/layout/Layout';
 import { PostPage } from '@/page-components';
-import axios from 'axios';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 const Users: FC = () => {
+	const { getComments } = useActions();
+	const [sort, setSort] = useState<SortType>('asc');
+	const [page, setPage] = useState<number>(1);
+	const [searchTitle, setSearchTitle] = useState<string>('');
 	const router = useRouter();
 	const { getPosts } = useActions();
-	const { posts, isLoading } = useAppSelector(state => state.posts);
+	const { posts, isLoading, pagesCount } = useAppSelector(state => state.posts);
+
 	useEffect(() => {
-		getPosts('3', '1', router.query.id as string);
+		getComments();
 	}, []);
-	if (isLoading) {
-		return <>Загрузка...</>;
-	}
-	return <PostPage posts={posts} />;
+
+	useEffect(() => {
+		if (searchTitle != '') {
+			getPosts(5, page, sort, searchTitle, router.query.id as string);
+		} else {
+			getPosts(5, page, sort, '', router.query.id as string);
+		}
+	}, [sort, page, searchTitle]);
+
+	return (
+		<PostPage
+			sort={sort}
+			setSort={setSort}
+			setSearchTitle={setSearchTitle}
+			posts={posts}
+			isLoading={isLoading}
+			page={page}
+			setPage={setPage}
+			pagesCount={pagesCount}
+		/>
+	);
 };
 
 export default withLayout(Users);
 
 export const getStaticPaths: GetStaticPaths = async () => {
 	let paths: string[] = [];
-	const { data: users } = await axios.get<IUser[]>(
-		'https://jsonplaceholder.typicode.com/users'
-	);
+	const { data: users } = await jsonApi.getUsers();
 	paths = paths.concat(users.flatMap(e => `/posts/${e.id}`));
 
 	return {
@@ -37,9 +58,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps<UsersProps> = async () => {
-	const { data: users } = await axios.get<IUser[]>(
-		'https://jsonplaceholder.typicode.com/users'
-	);
+	const { data: users } = await jsonApi.getUsers();
 	return {
 		props: {
 			users
