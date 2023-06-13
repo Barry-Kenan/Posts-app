1017c4d7b4f94cb6f90f3f5cc92a776e1fc1a8f9
Kenan <bary.berdiyev.97@mail.ru>
2023-06-13 09:03:45 +0700

    исправлен баг

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<meta property='og:locale' content='ru_RU' />
 			</Head>
-			<Component {...pageProps} />;
+			<Component {...pageProps} />
 		</>
 	);
 }
